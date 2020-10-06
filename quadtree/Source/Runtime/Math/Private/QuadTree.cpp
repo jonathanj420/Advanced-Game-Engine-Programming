dc5126d2b1a2b188c48c7124591ccdc11f10b5b6
jonathanj420 <48472233+jonathanj420@users.noreply.github.com>
2020-10-07 00:14:32 +0900

    quadtree

---
+++ quadtree/Source/Runtime/Math/Private/QuadTree.cpp
@@ -0,0 +1,90 @@
+#include "Precompiled.h"
+
+// 핵심 로직 
+bool QuadTree::Insert(const std::string& InKey, const Rectangle& InBound)
+{
+	if (!Bound.Intersect(InBound)) {
+		return false;
+	}
+
+	// 마지막 Depth까지 도달하면 자료구조에 추가하고 종료 { return true; }
+	if (Level >= MaxLevel) {
+		Nodes.emplace_back(InKey, InBound);
+		return true;
+	}
+
+	// 자식 노드를 만들어야 하는 경우 4개의 자식 노드를 새롭게 생성 => Split()
+	if (SubTrees[SubNames::TopLeft] == nullptr) {
+		Split();
+	}
+	// Split된 상태에서 FindSubTree 함수의 결과에 따라 처리를 다르게 진행
+	// 1. 겹쳐 있으면 자신의 자료구조에 넣고 끝내기 ( return true; )
+	QuadTree* newNode = FindSubTree(InBound);
+	if (newNode == nullptr) {
+		Nodes.emplace_back(InKey, InBound);
+		return true;
+	}
+	else {
+		newNode->Insert(InKey,InBound);
+	}
+	// 2. 안겹쳐져 있으면 자식으로 내려보내기 ( childNode->Insert ( 재귀 호출 ) )
+
+	
+}
+
+void CK::QuadTree::Clear()
+{
+}
+
+void CK::QuadTree::Query(const CK::Rectangle& InRectangleToQuery, std::vector<std::string>& InOutItems) const
+{
+}
+
+void QuadTree::Split()
+{
+	Vector2 center;
+	Vector2 extent;
+
+	Bound.GetCenterAndExtent(center, extent);
+
+
+	// SW, SE, NW, SE 네 개의 영역으로 쪼개고 이들의 바운딩 볼륨을 계산해서 넣기
+	SubTrees[SubNames::TopLeft] = new QuadTree(CK::Rectangle(Vector2(center.X-extent.X,center.Y),Vector2(center.X,center.Y+extent.Y)),Level+1,MaxLevel,NodeCapacity);
+
+	SubTrees[SubNames::TopRight] = new QuadTree(CK::Rectangle(center,center+extent),Level+1,MaxLevel,NodeCapacity);
+
+	SubTrees[SubNames::BottomRight] = new QuadTree(CK::Rectangle(Vector2(center.X,center.Y - extent.Y), Vector2(center.X + extent.X ,center.Y)), Level + 1, MaxLevel, NodeCapacity);
+
+	SubTrees[SubNames::BottomLeft] = new QuadTree(CK::Rectangle(center - extent, center), Level + 1, MaxLevel, NodeCapacity);
+
+}
+
+bool CK::QuadTree::Contains(const CK::Rectangle& InBox) const
+{
+	if (Bound.IsInside(InBox) == true) {
+
+		return true;
+
+	}
+	else {
+
+		return false;
+
+	}
+}
+
+QuadTree* QuadTree::FindSubTree(const Rectangle& InBound)
+{
+	// 네 개의 자식 노드를 돌면서 주어진 영역을 완전 포함하는 자식 트리가 있는지 조사.
+	for (QuadTree* subTree : SubTrees)
+	{
+		assert(subTree != nullptr);
+		if (subTree->Contains(InBound))
+		{
+			return subTree;
+		}
+	}
+
+	// 없으면 겹친다고 판단하고 null 반환
+	return nullptr;
+}
---
+++ quadtree/Source/Runtime/Math/Public/QuadTree.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <list>
+using namespace std;
+
+namespace CK
+{
+	class QuadTree
+	{
+	public:
+		QuadTree() = default;
+		QuadTree(const CK::Rectangle& InBound, int InLevel) : Bound(InBound), Level(InLevel) { }
+		QuadTree(const CK::Rectangle& InBound, int InLevel, int InMaxLevel, size_t InNodeCapacity) : Bound(InBound), Level(InLevel), MaxLevel(InMaxLevel), NodeCapacity(InNodeCapacity) { }
+		~QuadTree() { Clear(); }
+
+	private:
+		enum SubNames
+		{
+			TopLeft = 0,
+			TopRight = 1,
+			BottomRight = 2,
+			BottomLeft = 3
+		};
+
+		struct TreeNode
+		{
+			TreeNode() = delete;
+			TreeNode(const std::string& InNodeKey, const CK::Rectangle& InBound) : NodeKey(InNodeKey), NodeBound(InBound) { }
+
+			CK::Rectangle NodeBound;
+			std::string NodeKey;
+		};
+
+	public:
+		bool Insert(const std::string& InKey, const CK::Rectangle& InBound);
+		void Clear();
+		void Query(const CK::Rectangle& InRectangleToQuery, std::vector<std::string>& InOutItems) const;
+		bool HasItem() { return !IsLeaf; }
+
+	private:
+		void Split();
+		bool Contains(const CK::Rectangle& InBox) const;
+		QuadTree* FindSubTree(const CK::Rectangle& InBound);
+
+	private:
+		CK::Rectangle Bound;
+		std::list<TreeNode> Nodes;
+
+		bool IsLeaf = true;
+		int Level = 1;
+		int MaxLevel = 10;
+		size_t NodeCapacity = 6;
+
+		QuadTree* SubTrees[4] = { nullptr };
+	};
+}
