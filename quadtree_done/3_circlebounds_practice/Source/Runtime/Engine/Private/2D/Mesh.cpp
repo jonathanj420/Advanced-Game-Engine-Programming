91ee4814d0c7f678950c8a0fbcdc70961c342f18
jonathanj420 <48472233+jonathanj420@users.noreply.github.com>
2020-12-19 22:46:07 +0900

    quadtree_done

---
+++ quadtree_done/3_circlebounds_practice/Source/Runtime/Engine/Private/2D/Mesh.cpp
@@ -0,0 +1,10 @@
+
+#include "Precompiled.h"
+using namespace CK::DD;
+
+void Mesh::CalculateBounds()
+{
+	_CircleBound = Circle(_Vertices);
+	_RectangleBound = Rectangle(_Vertices);
+
+}
---
+++ quadtree_done/3_circlebounds_practice/Source/Runtime/Engine/Public/EngineHeaders.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <functional>
+
+#include "InputManager.h"
+#include "QuadTree.h"
+#include "2D/Mesh.h"
+#include "2D/Transform.h"
+#include "2D/GameObject.h"
+#include "2D/Camera.h"
+#include "2D/GameEngine.h"
+
+using namespace CK;
