d82d2d3b56fc8b1c3d8036800ce2862642128d9e
jonathanj420 <48472233+jonathanj420@users.noreply.github.com>
2020-12-19 22:45:10 +0900

    quadtree

--- quadtree_done/Source/Runtime/Engine/Private/2D/Mesh.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "Precompiled.h"
-using namespace CK::DD;
-
-void Mesh::CalculateBounds()
-{
-	_CircleBound = Circle(_Vertices);
-}
--- quadtree_done/Source/Runtime/Math/Private/Color32.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "Precompiled.h"
-
-const Color32 Color32::Error(255, 0, 255, 255);
